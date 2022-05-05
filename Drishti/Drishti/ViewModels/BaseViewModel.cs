821e1fc0702ce20d6640f58e6f7d572ffa68bd3e
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-05-05 23:52:03 +0530

    MediaPicker Plugin implementation

---
+++ Drishti/Drishti/ViewModels/BaseViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Drishti.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetPropertyValue<T>(ref T storageField, T newValue, [CallerMemberName] string propertyName = "")
+        {
+            if (Equals(storageField, newValue))
+                return false;
+
+            storageField = newValue;
+            RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /*protected bool SetPropertyValue<T>(ref T storageField, T newValue, Expression<Func<T>> propExpr)
+        {
+            if (Equals(storageField, newValue))
+                return false;
+
+            storageField = newValue;
+            var prop = (PropertyInfo)((MemberExpression)propExpr.Body).Member;
+            RaisePropertyChanged(prop.Name);
+
+            return true;
+        }
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propExpr)
+        {
+            var prop = (PropertyInfo)((MemberExpression)propExpr.Body).Member;
+            RaisePropertyChanged(prop.Name);
+        }*/
+    }
+}
---
+++ Drishti/Drishti/ViewModels/MediaPickerViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Drishti.ViewModels
+{
+    public class MediaPickerViewModel : BaseViewModel
+    {
+        private string photoPath;
+        private bool showPhoto;
+
+        public string PhotoPath
+        {
+            get { return photoPath; }
+            set { SetPropertyValue(ref photoPath, value); }
+        }
+
+        public bool ShowPhoto
+        {
+            get { return showPhoto; }
+            set { SetPropertyValue(ref showPhoto, value); }
+        }
+
+        public ICommand PickPhotoCommand { get; }
+        public ICommand CapturePhotoCommand { get; }
+
+
+        public MediaPickerViewModel()
+        {
+            PickPhotoCommand = new Command(DoPickPhoto);
+            CapturePhotoCommand = new Command(DoCapturePhoto, () => MediaPicker.IsCaptureSupported);
+        }
+
+        async void DoPickPhoto()
+        {
+            try
+            {
+                var photo = await MediaPicker.PickPhotoAsync();
+
+                await LoadPhoto(photo);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"PickPhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        async void DoCapturePhoto()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+
+                await LoadPhoto(photo);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {fnsEx.Message}");
+            }
+            catch (PermissionException pEx)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {pEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        async Task LoadPhoto(FileResult photo)
+        {
+            // cancelled
+            if (photo == null)
+            {
+                PhotoPath = null;
+                return;
+            }
+
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+            {
+                await stream.CopyToAsync(newStream);
+            }
+
+            PhotoPath = newFile;
+            ShowPhoto = true;
+        }
+    }
+}
