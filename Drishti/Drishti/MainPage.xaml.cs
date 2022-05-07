02ec556b8f64f0b654f6b0fa2f3438429cdea199
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-05-07 17:33:06 +0530

    using ViewModel for Data Binding

--- Drishti/Drishti/MainPage.xaml.cs
+++ Drishti/Drishti/MainPage.xaml.cs
@@ -7,49 +7,9 @@ namespace Drishti
 {
     public partial class MainPage : ContentPage
     {
-        public MediaFile image;
-
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private async void CameraButton_Clicked(object sender, EventArgs e)
-        {
-            await CrossMedia.Current.Initialize();
-
-            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-            {
-                await DisplayAlert("No Camera", ":( No camera available.", "OK");
-                return;
-            }
-
-            image = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
-            {
-                DefaultCamera = CameraDevice.Front,
-                PhotoSize = PhotoSize.Full,
-                SaveToAlbum = false
-            });
-
-            if (image == null)
-                return;
-
-            Image.Source = ImageSource.FromStream(() => image.GetStream());
-        }
-
-        private async void GalleryButton_Clicked(object sender, EventArgs e)
-        {
-            await CrossMedia.Current.Initialize();
-
-            image = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
-            {
-                PhotoSize = PhotoSize.Full
-            });
-
-            if (image == null)
-                return;
-
-            Image.Source = ImageSource.FromStream(() => image.GetStream());
         }
     }
 }
---
+++ Drishti/Drishti/ViewModels/MainViewModel.cs
@@ -0,0 +1,84 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Drishti.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private ImageSource _image;
+        public ImageSource Image
+        {
+            get => _image;
+            set
+            {
+                _image = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CapturePhotoCommand { get; }
+        public ICommand PickPhotoCommand { get; }
+
+        public MainViewModel()
+        {
+            PickPhotoCommand = new Command(PickPhoto, () => CrossMedia.Current.IsPickPhotoSupported);
+            CapturePhotoCommand = new Command(CapturePhoto, () => CrossMedia.Current.IsTakePhotoSupported);
+        }
+
+        private async void CapturePhoto()
+        {
+            try
+            {
+                var photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    DefaultCamera = CameraDevice.Front,
+                    PhotoSize = PhotoSize.Medium,
+                    SaveToAlbum = false
+                });
+
+                await LoadPhoto(photo);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                Console.WriteLine($"TakePhotoAsync THREW: {fnsEx.Message}");
+            }
+            catch (PermissionException pEx)
+            {
+                Console.WriteLine($"TakePhotoAsync THREW: {pEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"TakePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        private async void PickPhoto()
+        {
+            try
+            {
+                MediaFile photo = await CrossMedia.Current.PickPhotoAsync();
+                await LoadPhoto(photo);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"PickPhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        private async Task LoadPhoto(MediaFile photo)
+        {
+            // cancelled
+            if (photo == null)
+            {
+                return;
+            }
+
+            Image = ImageSource.FromStream(() => photo.GetStream());
+        }
+    }
+}
